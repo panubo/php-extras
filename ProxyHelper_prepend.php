a50ad9ed06afc17b4e6ed6f827f3a59d08ca2cb1
Andrew Cutler <andrew@panubo.com>
2018-08-13 11:58:01 +1000

    Read correct header HTTP_X_REAL_IP

--- ProxyHelper_prepend.php
+++ ProxyHelper_prepend.php
@@ -1,7 +1,7 @@
 <?php
 
 // Set IP correctly when being proxied
-$_SERVER['REMOTE_ADDR'] = $_SERVER['X-Real-IP'];
+$_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_X_REAL_IP'];
 
 // Set SSL Status correctly when being proxied
 if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
