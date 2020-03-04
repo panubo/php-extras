7889494cf30c5b119a0b8bf324cb594c4c4fc7c8
Andrew Cutler <andrew@panubo.com>
2020-03-05 10:19:37 +1100

    fix undefined index error

--- SSLHelper_prepend.php
+++ SSLHelper_prepend.php
@@ -1,7 +1,7 @@
 <?php
 
 // Set SSL Status correctly when being proxy
-if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
     $_SERVER['HTTPS'] = 'on';
     $_SERVER['REQUEST_SCHEME'] = 'https';
 }
