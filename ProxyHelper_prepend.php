fca32d34c298bf44e03ae8ec677288cc6ab015ae
Snow <snow@hol.ly>
2022-01-22 07:59:32 +1100

    Add tests similar to SSLHelper_prepend.

--- ProxyHelper_prepend.php
+++ ProxyHelper_prepend.php
@@ -1,10 +1,12 @@
 <?php
 
 // Set IP correctly when being proxied
-$_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_X_REAL_IP'];
+if (isset($_SERVER['HTTP_X_REAL_IP'])) {
+    $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_X_REAL_IP'];
+}
 
 // Set SSL Status correctly when being proxied
-if ($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
     $_SERVER['HTTPS'] = 'on';
     $_SERVER['REQUEST_SCHEME'] = 'https';
 }
